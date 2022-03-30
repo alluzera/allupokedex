83439d33c85f710839d325721e9195c9755441ee
Thiago Ribeiro <thiagoa.ribeir@gmail.com>
2022-03-30 05:13:11 -0300

    json update

--- app.js
+++ app.js
@@ -2,18 +2,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
     const updateViewWithPokemons = (pokemons) => {
        pokemons.forEach((pokemon) => {
         console.log(pokemon);
-        document.getElementById('apiName').textContent = pokemon.name
-        document.getElementById('apiDescription').textContent = pokemon.flavorText
-        document.getElementById('apiType').textContent = pokemon.types[0]
-        document.getElementById('apiNumber').textContent = pokemon.nationalPokedexNumbers[0]
-
-        console.log(pokemon.images.small)
      })
    }
-   
-   
-
-  fetch("https://api.pokemontcg.io/v2/cards?pageSize=5")
+     
+  fetch("https://raw.githubusercontent.com/alluzera/allupokedex/pokedex-API/pokestats.json")
     .then(response => response.json())
     .then(jsonResponse => updateViewWithPokemons(jsonResponse.data))
   });
