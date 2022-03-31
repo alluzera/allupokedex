1b2d7037dbfe9c4effe48e2813be7f0ab18ae911
Thiago Ribeiro <thiagoa.ribeir@gmail.com>
2022-03-30 22:21:09 -0300

    pokedex-API 0.2v finished

--- app.js
+++ app.js
@@ -1,15 +1,61 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    const updateViewWithPokemons = (pokemons) => {
-       pokemons.forEach((pokemon) => {
-        console.log(pokemon);
-     })
-   }
-     
-  fetch("https://raw.githubusercontent.com/alluzera/allupokedex/pokedex-API/pokestats.json")
+document.addEventListener("DOMContentLoaded", function (event) {
+  const myCustomDiv = document.createElement('div');
+  myCustomDiv.classList.add('pokedex');
+  const updateViewWithPokemons = (pokemons) => {
+    pokemons.forEach((pokemon) => {
+      console.log(pokemon);
+
+
+      const pokemonCard = document.createElement('div');
+
+      const pokeImg = document.createElement('img');
+      pokeImg.src = pokemon.img;
+      pokeImg.classList.add("pokemon-img");
+      
+
+      const pokeNumber = document.createElement('p');
+      if(pokemon.id < 10) {
+        pokeNumber.textContent = 'Nº00' + pokemon.id;
+      } else if(pokemon.id < 100) {
+        pokeNumber.textContent = 'Nº0' + pokemon.id;
+      } else {
+        pokeNumber.textContent = 'Nº' + pokemon.id;
+      }
+      pokeNumber.classList.add("pokemon-number");
+
+      const pokeName = document.createElement('p');
+      pokeName.textContent = pokemon.name;
+      pokeName.classList.add("pokemon-name");
+
+
+
+      pokemonCard.appendChild(pokeImg);
+      pokemonCard.appendChild(pokeNumber);
+      pokemonCard.appendChild(pokeName);
+
+      for (let i = 0; i < pokemon.type.length; i++) {
+        const pokeType = document.createElement('span');
+        pokeType.textContent = pokemon.type[i] + ' ';
+        pokemonCard.appendChild(pokeType);
+        pokeType.classList.add('badge', `badge-${pokemon.type[i]}`);
+      }
+      
+
+
+
+      myCustomDiv.appendChild(pokemonCard);
+
+    })
+
+    document.body.appendChild(myCustomDiv);
+  }
+
+
+
+  fetch("https://raw.githubusercontent.com/alluzera/allupokedex/pokedex-API/pokestats.json?pageSize=20")
     .then(response => response.json())
     .then(jsonResponse => updateViewWithPokemons(jsonResponse.data))
-  });
+});
 
-  
 
-  
+
