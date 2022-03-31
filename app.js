b39cf9609e5e29128fdfc5a606301b39bae84b75
Thiago Ribeiro <thiagoa.ribeir@gmail.com>
2022-03-30 22:39:15 -0300

    pokedex-API v0.2 finished

--- app.js
+++ app.js
@@ -1,61 +1,58 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-  const myCustomDiv = document.createElement('div');
-  myCustomDiv.classList.add('pokedex');
-  const updateViewWithPokemons = (pokemons) => {
-    pokemons.forEach((pokemon) => {
-      console.log(pokemon);
+document.addEventListener("DOMContentLoaded", function(event) {
+    const myCustomDiv = document.createElement('div');
+    myCustomDiv.classList.add('pokedex');
+    const updateViewWithPokemons = (pokemons) => {
+        pokemons.forEach((pokemon) => {
+            console.log(pokemon);
 
 
-      const pokemonCard = document.createElement('div');
+            const pokemonCard = document.createElement('div');
 
-      const pokeImg = document.createElement('img');
-      pokeImg.src = pokemon.img;
-      pokeImg.classList.add("pokemon-img");
-      
-
-      const pokeNumber = document.createElement('p');
-      if(pokemon.id < 10) {
-        pokeNumber.textContent = 'Nº00' + pokemon.id;
-      } else if(pokemon.id < 100) {
-        pokeNumber.textContent = 'Nº0' + pokemon.id;
-      } else {
-        pokeNumber.textContent = 'Nº' + pokemon.id;
-      }
-      pokeNumber.classList.add("pokemon-number");
-
-      const pokeName = document.createElement('p');
-      pokeName.textContent = pokemon.name;
-      pokeName.classList.add("pokemon-name");
+            const pokeImg = document.createElement('img');
+            pokeImg.src = pokemon.img;
+            pokeImg.classList.add("pokemon-img");
 
 
+            const pokeNumber = document.createElement('p');
+            if (pokemon.id < 10) {
+                pokeNumber.textContent = 'Nº00' + pokemon.id;
+            } else if (pokemon.id < 100) {
+                pokeNumber.textContent = 'Nº0' + pokemon.id;
+            } else {
+                pokeNumber.textContent = 'Nº' + pokemon.id;
+            }
+            pokeNumber.classList.add("pokemon-number");
 
-      pokemonCard.appendChild(pokeImg);
-      pokemonCard.appendChild(pokeNumber);
-      pokemonCard.appendChild(pokeName);
-
-      for (let i = 0; i < pokemon.type.length; i++) {
-        const pokeType = document.createElement('span');
-        pokeType.textContent = pokemon.type[i] + ' ';
-        pokemonCard.appendChild(pokeType);
-        pokeType.classList.add('badge', `badge-${pokemon.type[i]}`);
-      }
-      
+            const pokeName = document.createElement('p');
+            pokeName.textContent = pokemon.name;
+            pokeName.classList.add("pokemon-name");
 
 
 
-      myCustomDiv.appendChild(pokemonCard);
+            pokemonCard.appendChild(pokeImg);
+            pokemonCard.appendChild(pokeNumber);
+            pokemonCard.appendChild(pokeName);
 
-    })
+            for (let i = 0; i < pokemon.type.length; i++) {
+                const pokeType = document.createElement('span');
+                pokeType.textContent = pokemon.type[i] + ' ';
+                pokemonCard.appendChild(pokeType);
+                pokeType.classList.add('badge', `badge-${pokemon.type[i]}`);
+            }
 
-    document.body.appendChild(myCustomDiv);
-  }
 
 
 
-  fetch("https://raw.githubusercontent.com/alluzera/allupokedex/pokedex-API/pokestats.json?pageSize=20")
-    .then(response => response.json())
-    .then(jsonResponse => updateViewWithPokemons(jsonResponse.data))
+            myCustomDiv.appendChild(pokemonCard);
+
+        })
+
+        document.body.appendChild(myCustomDiv);
+    }
+
+
+
+    fetch("https://raw.githubusercontent.com/alluzera/allupokedex/pokedex-API/pokestats.json?pageSize=20")
+        .then(response => response.json())
+        .then(jsonResponse => updateViewWithPokemons(jsonResponse.data))
 });
-
-
-
